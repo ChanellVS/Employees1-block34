88a8ad3bbb61d1462d5917ea55f818bc48c5d592
ChanellVS <zariachanellb@gmail.com>
2025-07-01 00:06:31 -0400

    Employee-Part 2-Block 35 (COMPLETED)

--- api/employees.js
+++ api/employees.js
@@ -1,5 +1,101 @@
 import express from "express";
 const router = express.Router();
 export default router;
+import { createEmployee,getEmployees,getEmployee, updateEmployee, deleteEmployee } from "#db/queries/employees";
 
-// TODO: this file!
+//Route sends the message "Welcome to the fullstack Employees API"
+router.route("/").get(async (req, res)=> {
+    const message = "Welcome to the Fullstack Employees API."
+    res.send(message);
+})
+
+//Route send an array of all fullsatck employees
+router.route("/employees").get(async(req, res) =>{
+    const employees = await getEmployees();
+    res.send(employees);
+})
+
+//Route create and adds new employye to the fullstack employee table
+router.route("/employees").post(async(req, res) =>{
+    if(!req.body){
+       return res.status(400).send("Request body is not provided.")
+    }
+
+    const{name, birthday, salary} = req.body;
+
+    if(!name || !birthday || !salary){
+        return res.status(400).send("Request boddy is missing a required field.")
+    }
+
+    const employee = await createEmployee({name, birthday, salary});
+
+    res.status(201).send(employee);
+})
+
+
+
+router.route("/employees/:id").get(async(req, res) => {
+    const id = req.params.id
+
+    if(!Number.isInteger(id) && id < 0){
+        return res.status(400).send({error:"ID is not a positive number."})
+    }
+
+    const employee = await getEmployee(id);
+
+    if(!employee){
+        return res.status(404).send({error: "Employee was not found."})
+    }
+
+    res.send(employee);
+})
+
+
+
+router.route("/employees/:id").delete(async(req, res) => {
+    const id = req.params.id
+
+    if(!Number.isInteger(id) && id < 0){
+        return res.status(400).send({error:"ID is not a positive number."})
+    }
+
+    const employee = await getEmployee(id)
+
+    if(!employee){
+        return res.status(404).send({error: "Employee not found."})
+    }
+
+    if(employee){
+        const delEmp = await deleteEmployee(id)
+        return res.sendStatus(204)
+    }
+})
+
+
+router.route("/employees/:id").put(async(req, res) => {
+    const id = req.params.id
+
+    if(!req.body){
+        return res.status(400).send("Please provide request body.")
+    }
+
+    const{name, birthday, salary} = req.body
+
+    if(!name || !birthday || !salary){
+        return res.status(400).send("Please provide all params.")
+    }
+
+    if(!Number.isInteger(id) && id < 0){
+        return res.status(400).send({error:"ID is not a positive number."})
+    }
+
+    const employee = await getEmployee(id)
+
+    if(!employee){
+        return res.status(404).send({error: "Employee not found."})
+    }
+
+    const updated = await updateEmployee({id, name, birthday, salary})
+
+    res.status(200).send(updated)
+})
--- db/queries/employees.js
+++ db/queries/employees.js
@@ -2,18 +2,24 @@
 import db from "../client.js"
 
 // The parameters had "{}" around them, but it was proventing them from bing added to the table "employees".
-export async function createEmployee( name, birthday, salary) {
-  const result = await db.query(
-    'INSERT INTO employees (name, birthday, salary) VALUES ($1, $2, $3)RETURNING *;', [name, birthday, salary ]
-  )
-  return result
+export async function createEmployee( {name, birthday, salary} ) {
+  const sql = 
+    `INSERT INTO employees (name, birthday, salary) 
+    VALUES ($1, $2, $3) 
+    RETURNING * ;`
+    const {rows: employee} = await db.query(sql, [name, birthday, salary]);
+  
+  return employee[0];
 }
 
 // === Part 2 ===
 
 /** @returns all employees */
 export async function getEmployees() {
-  // TODO
+  const sql = `SELECT * FROM employees;`
+  const {rows: employees} = await db.query(sql);
+  return employees;
+  
 }
 
 /**
@@ -21,7 +27,9 @@ export async function getEmployees() {
  * @returns undefined if employee with the given id does not exist
  */
 export async function getEmployee(id) {
-  // TODO
+  const sql = `SELECT * FROM employees WHERE id = $1;`
+  const {rows: employee} = await db.query(sql, [id]);
+  return employee[0];
 }
 
 /**
@@ -29,7 +37,13 @@ export async function getEmployee(id) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
+  const sql = `UPDATE employees
+  SET name = $1, birthday = $2, salary = $3
+  WHERE id = $4
+  RETURNING *;
+  `
+  const {rows: employee} = await db.query(sql, [name, birthday, salary, id]);
+  return employee[0];
 }
 
 /**
@@ -37,5 +51,9 @@ export async function updateEmployee({ id, name, birthday, salary }) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function deleteEmployee(id) {
-  // TODO
+const sql = `
+DELETE FROM employees WHERE id = $1 RETURNING *;
+`
+ const {rows: employee} = await db.query(sql,[id]);
+ return employee;
 }
